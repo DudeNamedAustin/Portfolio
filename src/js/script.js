63dd2463b79317fbc55dbb175fc5cb6cc2bb964c
dudenamedaustin <austinlsmith35@gmail.com>
2019-02-21 00:55:18 -0600

    Background and Portfolio Section Updates

---
+++ src/js/script.js
@@ -0,0 +1,42 @@
+// Sal.js
+sal();
+
+// Glide JS
+new Glider(document.querySelector('.glider'), {
+  slidesToShow: 1,
+  scrollLock: true,
+  dots: '#dots',
+  draggable: false,
+  arrows: {
+    prev: '.glider-prev',
+    next: '.glider-next'
+  }
+});
+
+// Particles JS
+particlesJS.load('particles-js', 'src/assets/particles.json', function() {
+  console.log('I can see you');
+});
+
+
+// Auto Scroll JQuery
+$("#projects").click(function(e){
+  e.preventDefault();
+  $('html, body').animate({
+      scrollTop: $("#project").offset().top
+  }, 2000);
+});
+
+$("#contacts").click(function(e){
+  e.preventDefault();
+  $('html, body').animate({
+      scrollTop: $("#contact").offset().top
+  }, 2000);
+});
+
+$("#aboutMe").click(function(e){
+  e.preventDefault();
+  $('html, body').animate({
+      scrollTop: $("#about").offset().top
+  }, 2000);
+});
